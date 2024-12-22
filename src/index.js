3db57dee406b0b08fa9ebefa2afb3758c7ab61d4
nmsgames123 <shivam.tiwari@nmsgames.in>
2024-12-22 14:24:00 +0530

    create router-dom project for practice

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Root from './Root';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
+import User from './components/User';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Root />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path = "/" element = {<Root />} >
+      <Route path = "" element = {<Home />} />
+      <Route path = "about" element = {<About />} />
+      <Route path = "contact" element = {<Contact />} />
+      <Route path = "user/:userid" element = {<User />} />
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
